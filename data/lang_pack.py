7045d3696b716a767a0b9e9e4866376279ba4bee
Alex-Voinov <113195743+Alex-Voinov@users.noreply.github.com>
2023-03-10 14:25:35 +0300

    Add files via upload

---
+++ data/lang_pack.py
@@ -0,0 +1,18 @@
+VOCAB: dict = {
+            "Programm ended sucsessfully": (
+                "Программа завершена успешно",
+                "The program is completed successfully",
+                "Le programme endo sucsesion",
+            ),
+            "New path for your database is made, restart the application to continue": (
+                'Создан новый путь для вашей базы данных, перезапустите приложение, чтобы продолжить',
+                "New path for your database is made, restart the application to continue",
+                "Nuovo percorso per il database è fatto, riavviare l'applicazione per continuare",
+            )
+        }
+
+LANGCODE: dict = {
+    '1': 'Русский язык',
+    '2': 'English language',
+    '3': 'Italiano',
+}
